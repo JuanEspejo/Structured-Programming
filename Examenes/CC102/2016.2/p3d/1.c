ae628b4965352cf377b3e1509199389388e641e8
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2017-03-07 17:15:12 -0500

    Create 1.c

---
+++ Examenes/CC102/2016.2/p3d/1.c
@@ -0,0 +1,7 @@
+a) Se declara un puntero a un arreglo de 5 enteros.
+
+b) La razón de ser de ellas es transformar la expresión ptr a un puntero a entero, la misma naturaleza que la variable p.
+
+c)  (i)   función main: imprime 2 veces las entradas de 'la matriz' multi. La primera llamando 3 veces la función imprimir_arreglo_1 y la segunda llamando sólo una vez a la función imprimir_arreglo_2
+    (ii)  función imprimir_arreglo_1: imprime todos los elementos del arreglo al cual apunta el parámetro que se le pasa.
+    (iii) función imprimir_arreglo_2: imprime el primer elemento del arreglo al cual apunta el parámetro que se le pasa y luego imprime los 14 enteros siguientes a este.
