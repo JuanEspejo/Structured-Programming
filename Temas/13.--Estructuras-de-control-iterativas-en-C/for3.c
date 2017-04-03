c933284b5e62ee94d4126048ae51a8228817681b
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2017-04-03 17:02:25 -0500

    Update for3.c

--- Temas/13.--Estructuras-de-control-iterativas-en-C/for3.c
+++ Temas/13.--Estructuras-de-control-iterativas-en-C/for3.c
@@ -1,10 +1,10 @@
 /* Programa que muestra el anidamiento de dos sentencias "for" */
 #include <stdio.h>
 
-int x, y; // para almacenar las coordenadas
-
 void main( void )
 {
+	int x, y; // para almacenar las coordenadas
+	
 	for (y = 0; y >= -2; y--)
 	{
 		for (x = 0; x <= 4; x++)
