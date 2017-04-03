d3bd16581a223560ff3dc7879aa29c59b63e5bc0
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2017-04-03 17:12:33 -0500

    Update rombo.c

--- Temas/13.--Estructuras-de-control-iterativas-en-C/rombo.c
+++ Temas/13.--Estructuras-de-control-iterativas-en-C/rombo.c
@@ -3,13 +3,14 @@
 void main()
 {
 	int n,x,y,h;
-    	printf("n = ");
-   	scanf("%d",&n);
-    	h = (n-1)/2;
-    	for (y=-h; y <= h ; y++)
-    	{
-        	for (x=h; x >= -h; x--)
-            		if ( (abs(x) + abs(y)) <= h ) printf("*"); else printf(" ");
-        	printf("\n");
-    	}
+	printf("n = ");
+	scanf("%d",&n);
+	h = (n-1)/2;
+	
+	for (y=-h; y <= h ; y++)
+	{
+		for (x=h; x >= -h; x--)
+		    if ( (abs(x) + abs(y)) <= h ) printf("*"); else printf(" ");
+		printf("\n");
+	}
 }
