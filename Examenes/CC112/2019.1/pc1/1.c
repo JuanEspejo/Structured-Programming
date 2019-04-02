4c1e7f992dfcac662f78cb15abcb745ee6e861e6
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-04-02 11:16:24 -0500

    Delete 1.c

--- Examenes/CC112/2019.1/pc1/1.c
+++
@@ -1,9 +0,0 @@
-//1.c
-#include<cstdio>
-main(){
-	printf("Promedio de prácticas\n");
-	int p1=13, p2=17, p3=14, p4, suma;
-	suma = p1+p2+p3;
-	p4 = 20-suma%4;
-	printf("P1 = %d, P2 = %d, P3 = %d, P4 = %d, Promedio = %d\n", p1, p2, p3, p4, (p1+p2+p3+p4)/4);
-}
