2a946a53c4aa48fa70772556af975074820bd313
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-04-13 12:22:55 -0500

    Create multiplos2.cpp

---
+++ Temas/12.--Estructuras-de-control-selectivas-en-C/multiplos2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+main () 
+{
+	int n;
+	
+	cout << " Ingrese un numero: ";
+	cin >> n;
+	
+	cout << " Es multiplo de ";
+	if(n%2 == 0) 
+	{
+		cout << "dos"; 
+	}
+	else
+	{
+		if(n%3 == 0)
+		{
+			cout << "tres";
+		} 
+		else
+		{
+			if(n%5 == 0) 
+			{
+				cout << "cinco"; 
+			}
+			else
+			{
+				cout << "siete";
+			}
+		}
+	}	 
+	cout << endl;
+}
