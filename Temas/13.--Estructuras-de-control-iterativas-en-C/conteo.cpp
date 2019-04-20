744c37d9d9ef326c747da25c8b60f9d670d3b989
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-04-20 13:50:52 -0500

    Create conteo.cpp

---
+++ Temas/13.--Estructuras-de-control-iterativas-en-C/conteo.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+main ()
+{
+	int contador = 0;
+	
+	do 
+	{
+		contador = contador + 1;
+		cout << contador << endl;
+	} while (contador <= 9);
+}
+
+
+
+
+
