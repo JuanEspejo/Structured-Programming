49c3b90e647d43596a775804d485d2b205d63bba
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-04-27 11:38:05 -0500

    Create dibujo1.cpp

---
+++ Temas/13.--Estructuras-de-control-iterativas-en-C/dibujo1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+main ()
+{
+	int j = 0;
+
+	while (j <= 6)
+	{
+		if(j%2 == 0)
+			cout << "*";
+		else
+			cout << "+";
+		cout << endl;
+		j = j + 1;
+	} 
+	
+}
+
+
+
+
+
