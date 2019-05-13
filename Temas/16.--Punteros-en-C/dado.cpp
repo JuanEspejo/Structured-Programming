d42787905716fda61067574f18e3c68ab0979a6b
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-05-13 15:35:35 -0500

    Update dado.cpp

--- Temas/16.--Punteros-en-C/dado.cpp
+++ Temas/16.--Punteros-en-C/dado.cpp
@@ -4,15 +4,14 @@
 using namespace std;
 
 int main() {
-	int dice[20];
 	int frequency[7] = {0,0,0,0,0,0,0};
 	
 	for(int j=0; j<20; j++) {
-		*(dice + j) = 1 + rand()%6;
-		*(frequency + *(dice + j)) += 1;
+		int dice = 1 + rand()%6;
+		*( frequency + dice ) += 1;
 	}	
 
 	for(int j=1; j<7; j++){
-		cout << " frecuency["<<j<<"] : "<< dice[j] << endl;
+		cout << " frecuency["<<j<<"] : "<< *(frequency + j) << endl;
 	}	
 }
