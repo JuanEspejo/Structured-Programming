4837915b47b037bf31ee458ca8ca57a8120290f4
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-06-08 14:35:33 -0500

    Update hola2.cpp

--- Temas/17.--Cadenas-de-caracteres-en-C/hola2.cpp
+++ Temas/17.--Cadenas-de-caracteres-en-C/hola2.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main(){
-	char hola[20] = {'H',111,'l',97,' ','m',117,'n',100,'o',33,0};
+	char hola[20] = {'H',111,'l',97,' ','m',117,'n',100,'o',33};
 	cout << hola << endl;
 }
