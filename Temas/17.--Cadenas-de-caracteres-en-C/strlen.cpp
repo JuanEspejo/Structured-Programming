a65583d04c7793c762f4b8d498ae8dacabcf7f7c
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-06-08 14:49:28 -0500

    Create strlen.cpp

---
+++ Temas/17.--Cadenas-de-caracteres-en-C/strlen.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	char saludo[50] = "Hola ,mucho gusto; muy buenas tardes";
+	
+	cout << "saludo: " << saludo << endl;
+	cout << "strlen(saludo): " << strlen(saludo) << endl;
+}
