43cbd88172812a8475bf40b930eae9ed098a58a7
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-06-17 15:35:45 -0500

    Update Coordenada.cpp

--- Temas/19.--Estructuras-de-datos/Coordenada.cpp
+++ Temas/19.--Estructuras-de-datos/Coordenada.cpp
@@ -1,12 +1,13 @@
 #include <cstdio>
 
-struct coordenada{
+typedef struct {
     int x;
     int y;
-} punto;
+} Punto;
 
 int main(){
     /* asignando valores a las coordenadas de punto */
+    Punto punto;
     punto.x = 5;
     punto.y = 12;
     printf("\nLas coordenadas son: (%d,%d).\n\n", punto.x, punto.y);
