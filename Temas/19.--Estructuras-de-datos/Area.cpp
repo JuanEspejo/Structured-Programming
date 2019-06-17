613b1b15a34fedfb41ff1bcb965e1970faedd3a8
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-06-17 15:34:32 -0500

    Update Area.cpp

--- Temas/19.--Estructuras-de-datos/Area.cpp
+++ Temas/19.--Estructuras-de-datos/Area.cpp
@@ -1,34 +1,35 @@
 #include <cstdio>
 #include <cstdlib>
 
-int delta1, delta2;
-long area;
-struct coordenada{
+typedef struct {
     int x;
     int y;
-};
+} Coordenada;
 
-struct rectangulo{
-    struct coordenada esquina1;
-    struct coordenada esquina2;
-} rectangulo1;
+typedef struct {
+    Coordenada esquina1;
+    Coordenada esquina2;
+} Rectangulo;
 
 int main(){
+    Rectangulo rectangulo;
+    int delta1, delta2;
+    long area;
     /* Ingresando las coordenadas de esquina1 */
     printf("\n Ingrese la abcisa de esquina1: ");
-    scanf("%d", &rectangulo1.esquina1.x);
+    scanf("%d", &rectangulo.esquina1.x);
     printf("Ingrese la ordenada de esquina1: ");
-    scanf("%d", &rectangulo1.esquina1.y);
+    scanf("%d", &rectangulo.esquina1.y);
 
     /* Ingresando las coordenadas de esquina2 */
     printf("\nIngrese la abcisa de esquina2: ");
-    scanf("%d", &rectangulo1.esquina2.x);
+    scanf("%d", &rectangulo.esquina2.x);
     printf("Ingrese la ordenada de esquina2: ");
-    scanf("%d", &rectangulo1.esquina2.y);
+    scanf("%d", &rectangulo.esquina2.y);
 
     /* Calculando y mostrando el \'area */
-    delta1 = abs(rectangulo1.esquina1.x - rectangulo1.esquina2.x);
-    delta2 = abs(rectangulo1.esquina1.y - rectangulo1.esquina2.y);
+    delta1 = abs(rectangulo.esquina1.x - rectangulo.esquina2.x);
+    delta2 = abs(rectangulo.esquina1.y - rectangulo.esquina2.y);
     area = delta1*delta2;
     printf("\nEl area es de %ld unidades.\n\n", area);
 }
