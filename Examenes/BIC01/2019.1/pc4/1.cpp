b22e22363fc9588b681d12ff26dfa1857650d5cb
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-06-24 17:10:19 -0500

    Create 1.cpp

---
+++ Examenes/BIC01/2019.1/pc4/1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	int indice,x;
+	char frase[60] ; /* Define arreglo de caracteres */
+	printf("Ingrese la frase :");
+	fgets(frase, 60, stdin);
+	printf("%s" ,frase); /* Imprime la cadena frase */
+	printf("\n");
+	for(x = 0;x < 60;x++)
+	{
+		if (frase[x]=='\0')
+		{
+			indice = x;
+			break;
+		}
+	}
+	printf("La ultima letra es: \n");
+	printf("%c",frase[indice-2]);
+	printf("\n\n");
+	return 0;
+}
