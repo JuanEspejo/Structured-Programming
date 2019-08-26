e83b4cbef509c81139efe9f0356c4d4858d6b515
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-08-26 17:27:56 -0500

    Create puntero.cpp

---
+++ Temas/14.--Funciones-en-C/puntero.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+	int nota = 20;
+	int * puntero = &nota;
+
+	cout << "nota: " << nota << endl;
+	cout << "&nota: " << &nota << endl;
+	cout << "puntero: " << puntero << endl;
+}
+
+
+
+
+
