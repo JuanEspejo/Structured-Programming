cdee301426447dde01f4f2c653b77bf45db18917
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-08-26 17:19:54 -0500

    Create memoria.cpp

---
+++ Temas/14.--Funciones-en-C/memoria.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+	int nota = 20;
+
+	cout << "nota: " << nota << endl;
+	cout << "&nota: " << &nota << endl;
+	cout << "sizeof(nota): " << sizeof(nota) << endl;
+	cout << "sizeof(int): " << sizeof(int) << endl;
+	cout << "sizeof(char): " << sizeof(char) << endl;
+}
