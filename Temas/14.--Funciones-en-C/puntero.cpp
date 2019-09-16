4365f9e65a56d6854845310145bdde6e5b3d9a05
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-09-16 12:50:26 -0500

    Update puntero.cpp

--- Temas/14.--Funciones-en-C/puntero.cpp
+++ Temas/14.--Funciones-en-C/puntero.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main ()
 {
-	int nota = 20;
-	int * puntero = &nota; // & es el operador unario direccion
+    int nota = 20;
+    int * puntero = &nota; // & es el operador unario direccion
 
-	cout << "nota: " << nota << endl;
-	cout << "&nota: " << &nota << endl;
-	cout << "puntero: " << puntero << endl;
+    cout << "nota: " << nota << endl;
+    cout << "nota: " << *puntero << endl;
+    cout << "&nota: " << &nota << endl;
+    cout << "puntero: " << puntero << endl;
 }
-
