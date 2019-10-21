20d92f3ba30a26322775eff7a994d1022f5c9ed9
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-10-21 16:39:36 -0500

    Update exe_5.cpp

--- Temas/17.--Cadenas-de-caracteres-en-C/exe_5.cpp
+++ Temas/17.--Cadenas-de-caracteres-en-C/exe_5.cpp
@@ -1,37 +1,48 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-
 #define TAM 51
 void generar(char * c, int r);
+void convertir(char *c, int r);
 void mostrar(char * c, int r);
 
 int main(){
     srand(time(NULL));
     char cad[TAM];
-    int tam = 50;
+    int tam=50;
     generar(cad, tam);
-    mostrar(cad, tam);
+    mostrar(cad,tam);
+
+    convertir(cad,tam);
+    mostrar(cad,tam);
 }
 
-void generar(char *c, int r)
-{
+void generar(char * c, int r){
     int m;
-    for(int j = 0; j < r; j++){
+    for (int i = 0; i < r; i++) {
         m = rand()%53;
         if(m == 0)
-            c[j] = 32;
+            c[i] = 32;
         else if(m < 27)
-            c[j] = m + 64;
+            c[i] = m + 64;
         else
-            c[j] = m + 70;
+            c[i] = m + 70;
     }
-    c[r] = 0;
+    c[r]=0;
+}
+
+void convertir(char *c, int r)
+{
+    for (int i = 0; i < r; i++)
+    {
+        if (c[i]>=97 && c[i]<=122)
+            c[i]= c[i] - 32;
+    }
 }
 
 void mostrar(char * c, int r){
-    for(int j = 0; j < r; j++){
-        cout << c[j];
+    for (int i = 0; i < r; i++) {
+        cout << c[i];
     }
-    cout << ":FINAL" << endl;
+    cout << ":FIN" << endl;
 }
