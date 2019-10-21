48b65585e117fa2c279ff6f6c0af38dfd08df7ba
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-10-21 13:08:11 -0500

    Update Palindromo2.cpp

--- Temas/17.--Cadenas-de-caracteres-en-C/Palindromo2.cpp
+++ Temas/17.--Cadenas-de-caracteres-en-C/Palindromo2.cpp
@@ -1,37 +1,29 @@
 #include<iostream>
 #include<cstring>
-#include<cstdio>
 
 using namespace std;
 
 int main(){
-	char frase[50];
-	char palabra[50];
-	int k = 0;
-	
-    cout << "ingrese una palabra: ";
-	fgets(frase,50,stdin);   
-	cout << "frase: " << frase << endl;
-    int iFinal = strlen(frase) - 2; 
-	cout << "iFinal: " << iFinal << endl; // amad a la dama
-    // iFinal: 13
-    // frase: "amadal"
+    char frase[50] = "Anita lava la tina";
+    char palabra[50];
 
+    int k = 0;
+    int iFinal = strlen(frase) - 1;
     for (int j = 0; j <= iFinal; j++) {
         if (frase[j] != ' ') {
             palabra[k] = frase[j];
             k++;
-        }    
+        }
     }
     palabra[k] = 0;
 
-	cout << "palabra: " << palabra << endl;    
+    cout << "palabra: " << palabra << endl;
     iFinal = strlen(palabra) - 1;
-	for (int j = 0; j <= iFinal/2; j++) {
-        if (palabra[j] != palabra[iFinal - j]) {
-			cout << "NO es palindromo!" << endl;
-			return 0;
-		}
+    for (int j = 0; j <= iFinal/2; j++) {
+        if ((palabra[j] - palabra[iFinal - j])%32 != 0) {
+            cout << "NO es palindromo!" << endl;
+            return 0;
+        }
     }
     cout << "Es palindromo!" << endl;
 
