d1c35f61148d996bcd10363f104e26118fdf710e
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-10-21 21:32:48 -0500

    Update exe_8.cpp

--- Temas/17.--Cadenas-de-caracteres-en-C/exe_8.cpp
+++ Temas/17.--Cadenas-de-caracteres-en-C/exe_8.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void generar_saludo(char (*des)[COL], char (*ori)[COL]);
+void generar_saludo(char (*des)[COL], char (*ori)[COL], int filas);
 void mostrar(char (*ptr)[COL], int filas);
 
 int main()
@@ -18,14 +18,19 @@ int main()
 		"picka picka",
 		"pickachu"
 		};
+	cout << "diccionario:" << endl;
 	mostrar(diccionario, FIL);
-	generar_saludo(pickachu, diccionario)
+	generar_saludo(pickachu, diccionario, FIL);
+	cout << "pickachu:" << endl;		
 	mostrar(pickachu, FIL);
 }
 
-void generar_saludo(char (*des)[COL], char (*ori)[COL])
+void generar_saludo(char (*des)[COL], char (*ori)[COL], int filas)
 {
-	;
+	for (int i = 0; i < filas; i++)
+	{
+		strcpy(des[i],ori[filas-1-i]);
+	}	
 }
 
 void mostrar(char (*ptr)[COL], int filas)
