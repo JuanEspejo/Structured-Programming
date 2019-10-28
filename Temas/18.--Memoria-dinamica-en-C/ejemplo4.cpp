f78bba2d25ffc424ee10fbbccc5e2414a74b3b51
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-10-28 13:43:51 -0500

    Update ejemplo4.cpp

--- Temas/18.--Memoria-dinamica-en-C/ejemplo4.cpp
+++ Temas/18.--Memoria-dinamica-en-C/ejemplo4.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
 #include <cstdlib>
-#include<iomanip> 
+#include<iomanip>
 
 using namespace std;
 
 int main ()
 {
-	int * notas;
-	int k, i, r;
+    int * notas;
+    int k, i, r;
 
-	cout << " ingrese la longitud del arreglo: ";
-	cin >> k;
-	notas = (int *)malloc( k*sizeof(int) );
-	
-	if (notas == NULL)
-	{
-		cout << " NO se pudo asignar memoria" << endl;
-		return 1;
-	}
-	
-	cout << " se ha creado un arreglo de enteros de longitud " << k << endl;
+    cout << " ingrese la longitud del arreglo de enteros: ";
+    cin >> k;
+    notas = (int *)malloc( k*sizeof(int) );
 
-	cout << " ingrese la nueva longitud del arreglo: ";
-	cin >> r;
-	notas = (int *)realloc(notas, r*sizeof(int));
+    if (notas == NULL)
+    {
+        cout << " NO se pudo asignar memoria" << endl;
+        return 1;
+    }
 
-	if (notas == NULL)
-	{
-		cout << " NO se pudo reasignar memoria" << endl;
-		return 1;
-	}
-	
-	cout << " el arreglo de enteros ahora tiene longitud " << r << endl;
+    cout << " se ha creado un arreglo de enteros de longitud " << k << endl;
 
-	free (notas); // liberando
+    cout << " ingrese la nueva longitud del arreglo creado arriba: ";
+    cin >> r;
+    notas = (int *)realloc(notas, r*sizeof(int));
+
+    if (notas == NULL)
+    {
+        cout << " NO se pudo reasignar memoria" << endl;
+        return 1;
+    }
+
+    cout << " el arreglo de enteros ahora tiene longitud " << r << endl;
+
+    free (notas); // liberando
 }
