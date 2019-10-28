c27a1e0515f137116fba46391f6e2ee73cf7fcae
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-10-28 12:49:40 -0500

    Update ejemplo2.cpp

--- Temas/18.--Memoria-dinamica-en-C/ejemplo2.cpp
+++ Temas/18.--Memoria-dinamica-en-C/ejemplo2.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <cstdlib>
- 
-using namespace std; 
- 
+
+using namespace std;
+
 int main ()
 {
-	int * notas;
-	size_t k;
+    int * notas;
+    size_t k;
 
-	cout << " ingrese la longitud del arreglo: ";
-	cin >> k;
-	notas = (int *) malloc( k*sizeof(int) );
+    cout << " ingrese la longitud del vector de enteros: ";
+    cin >> k;
+    notas = (int *) malloc( k*sizeof(int) );
 
-	if (notas == NULL)
-	{
-		cout << " NO se pudo asignar memoria." << endl;
-		return 1;
-	}
-	
-	cout << " se ha asignado un arreglo de longitud " << k << endl;
-	cout << " notas  : " << notas << endl;
-	cout << " notas+1: " << notas+1 << endl;
-	
-	free (notas); // liberando
+    if (notas == NULL)
+    {
+        cout << " NO se pudo asignar memoria." << endl;
+        return 1;
+    }
+
+    cout << " se ha asignado un arreglo de longitud " << k << endl;
+    cout << " notas  : " << notas << endl;
+    cout << " notas+1: " << notas+1 << endl;
+
+    free (notas); // liberando
 }
