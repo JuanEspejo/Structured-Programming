d9a4f9a099f125a72089d6671a8131be1a968aa2
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-11-11 12:49:14 -0500

    Create Coordenada2.cpp

---
+++ Temas/19.--Estructuras-de-datos/Coordenada2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct {
+    int x;
+    int y;
+} Punto;
+
+int main(){
+    Punto punto1; // int nota;
+    /* asignando valores a las coordenadas de punto */
+    punto1.x = 5;
+    punto1.y = 12;
+    cout << "Las coordenadas son: (" << punto.x << ",";
+    cout << punto.y << ")." << endl;
+
+    Punto punto2; // int nota;
+    punto2.x = 11;
+    punto2.y = 60;
+    cout << "Las coordenadas son: (" << punto.x << ",";
+    cout << punto.y << ")." << endl;
+}
