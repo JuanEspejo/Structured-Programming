b4e7be7426fccbeb0d7181dbb9aa8bac53dc4700
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-11-17 21:40:10 -0500

    Update ejemplo3.cpp

--- Temas/20.--Manejo-de-archivos-en-C/ejemplo3.cpp
+++ Temas/20.--Manejo-de-archivos-en-C/ejemplo3.cpp
@@ -3,14 +3,14 @@
 int main() 
 {
 	// definiendo un puntero a FILE
-	FILE *fp;
+	FILE *pf;
 	
 	// abre el archivo prueba.txt para escribir
-	if((fp = fopen("prueba.txt", "a")) != NULL) 
+	if((pf = fopen("prueba.txt", "a")) != NULL) 
 	{
-		fprintf(fp, "%d %s %f\n", 1, "Pedro", 1.7);
-		fprintf(fp, "%d %s %f\n", 2, "Paul", 3.1);
-		fclose(fp); // cierra el archivo
+		fprintf(pf, "%d %s %f\n", 1, "Pedro", 1.7);
+		fprintf(pf, "%d %s %f\n", 2, "Paul", 3.1);
+		fclose(pf); // cierra el archivo
 	} 
 	else printf("No se pudo abrir el archivo.\n");
 }
