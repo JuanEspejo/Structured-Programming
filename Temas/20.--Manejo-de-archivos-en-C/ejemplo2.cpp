ff0dafb4e4389bd863068ac89b8a9b875323bf43
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-11-17 21:39:41 -0500

    Update ejemplo2.cpp

--- Temas/20.--Manejo-de-archivos-en-C/ejemplo2.cpp
+++ Temas/20.--Manejo-de-archivos-en-C/ejemplo2.cpp
@@ -6,14 +6,14 @@ int main()
 	int item;
 	char nombre[10];
 	float monto;
-	FILE *fp;
+	FILE *pf;
 
-	if((fp = fopen("prueba.txt", "r")) == NULL)
+	if((pf = fopen("prueba.txt", "r")) == NULL)
 	{
 		printf("No se pudo abrir el archivo.\n"); 
 		exit(1);
 	}
-	while(fscanf(fp, "%d %s %f", &item, nombre, &monto) > 0) 
+	while(fscanf(pf, "%d %s %f", &item, nombre, &monto) > 0) 
 		printf("%d %s %f\n", item, nombre, monto);
-	fclose(fp);
+	fclose(pf);
 }
