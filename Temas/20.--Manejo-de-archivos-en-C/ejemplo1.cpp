77c28d5813ac1b8ae44f1180c974f7877da4eff3
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-11-17 20:56:05 -0500

    Create ejemplo1.cpp

---
+++ Temas/20.--Manejo-de-archivos-en-C/ejemplo1.cpp
@@ -0,0 +1,16 @@
+#include<cstdio>
+
+int main() 
+{
+	// definiendo un puntero a FILE
+	FILE *fp;
+	
+	// abre el archivo prueba.txt para escribir
+	if((fp = fopen("prueba.txt", "w")) != NULL) 
+	{
+		fprintf(fp, "%d %s %f\n", 1, "Pedro", 1.7);
+		fprintf(fp, "%d %s %f\n", 2, "Paul", 3.1);
+		fclose(fp); // cierra el archivo
+	} 
+	else printf("No se pudo abrir el archivo.\n");
+}
